321f7d0ad7c0be434a4c46f84548de93aade0f1a
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-07-06 16:08:30 -0500

    feat: add skills using tab grid molecule

--- components/templates/Home.tsx
+++ components/templates/Home.tsx
@@ -2,8 +2,37 @@ import Hero from "@/components/organisms/Hero";
 import { getTranslations } from "next-intl/server";
 import Heading from "@/components/atoms/Heading";
 import Description from "@/components/atoms/Description";
+import TagGrid from "@/components/molecules/TagGrid";
+import Tag from "@/components/atoms/Tag";
+
+type Tag = React.ComponentProps<typeof Tag>;
+
 export default async function Home() {
   const t = await getTranslations("HomePage");
+
+  const skills: Tag[] = [
+    { label: t("skills.python"), variant: "primary" },
+    { label: t("skills.php"), variant: "primary" },
+    { label: t("skills.javascript"), variant: "primary" },
+    { label: t("skills.typescript"), variant: "primary" },
+    { label: t("skills.frontend"), variant: "primary" },
+    { label: t("skills.git"), variant: "primary" },
+    { label: t("skills.github"), variant: "primary" },
+    { label: t("skills.azure"), variant: "primary" },
+    { label: t("skills.mysql"), variant: "primary" },
+    { label: t("skills.django"), variant: "primary" },
+    { label: t("skills.laravel"), variant: "primary" },
+    { label: t("skills.nextjs"), variant: "primary" },
+    { label: t("skills.n8n"), variant: "primary" },
+    { label: t("skills.powerAutomate"), variant: "primary" },
+    { label: t("skills.docker"), variant: "primary" },
+    { label: t("skills.teamwork"), variant: "secondary" },
+    { label: t("skills.leadership"), variant: "secondary" },
+    { label: t("skills.communication"), variant: "secondary" },
+    { label: t("skills.continuousLearning"), variant: "secondary" },
+    { label: t("skills.problemSolving"), variant: "secondary" },
+    { label: t("skills.adaptability"), variant: "secondary" },
+  ];
 
   return (
     <div className="">
@@ -19,11 +48,19 @@ export default async function Home() {
           imageAlt={t("hero.imageAlt")}
         />
       </section>
+
       <section id="profile" className="my-10">
         <Heading variant="h3" className="my-5">
           {t("profile.title")}
         </Heading>
         <Description>{t("profile.description")}</Description>
+      </section>
+
+      <section id="skills" className="my-10">
+        <Heading variant="h3" className="my-5">
+          {t("skills.title")}
+        </Heading>
+        <TagGrid tags={skills}></TagGrid>
       </section>
     </div>
   );
