fc7bfc6b1463d1dde766a2114c5476c986257a34
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-07-06 15:13:59 -0500

    feat: add home template with hero section

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
+import "@/styles/globals.css";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { JSX } from "react";
+import { routing } from "@/i18n/routing";
+import { Locale } from "@/i18n/routing";
+import { notFound } from "next/navigation";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+});
+
+interface LocaleProps {
+  children: React.ReactNode;
+  params: { locale: string };
+}
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: LocaleProps): Promise<JSX.Element> {
+  const { locale } = await params;
+
+  if (!routing.locales.includes(locale as Locale)) {
+    notFound();
+  }
+
+  const messages = await getMessages();
+
+  return (
+    <html lang="en">
+      <body
+        className={`${spaceGrotesk.className} antialiased mx-[20px] sm:mx-[40px] md:mx-[60px] lg:mx-[80px] xl:mx-[100px] 2xl:mx-[150px] bg-[#111214] text-zinc-200`}
+      >
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ styles/customs.ts
@@ -0,0 +1,8 @@
+export const baseHeadingStyles = {
+  h1: "text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold",
+  h2: "text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold",
+  h3: "text-xl  sm:text-2xl md:text-3xl lg:text-4xl font-semibold",
+  h4: "text-lg  sm:text-xl  md:text-2xl lg:text-3xl font-medium",
+  h5: "text-base sm:text-lg  md:text-xl  lg:text-2xl font-medium",
+  h6: "text-sm  sm:text-base md:text-lg  lg:text-xl font-medium",
+};
