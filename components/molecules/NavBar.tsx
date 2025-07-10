c757d3104304dae769238dd13d0babb85de83148
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-07-09 21:18:12 -0500

    fix: correct responsive styles from navbar

--- components/molecules/NavBar.tsx
+++ components/molecules/NavBar.tsx
@@ -9,8 +9,8 @@ export default function NavBar(): JSX.Element {
   const { githubUrl, linkedinUrl } = data.Navbar;
   return (
     <nav className="my-5 text-white border-b border-gray-700 bg-[#121417]">
-      <div className="container mx-auto px-4 py-2 flex justify-between items-center">
-        <div className="text-xl font-bold">{personalEmail}</div>
+      <div className="container mx-auto px-4 py-2 flex justify-center flex-col sm:justify-between sm:flex-row items-center">
+        <div className="text-xl font-bold mx-2 mb-2 sm:mb-0 sm:mx-0">{personalEmail}</div>
         <div className="flex space-x-4">
           <ButtonLinkIcon href={githubUrl}>{<GithubIcon />}</ButtonLinkIcon>
           <ButtonLinkIcon href={linkedinUrl}>{<LinkedinIcon />}</ButtonLinkIcon>
